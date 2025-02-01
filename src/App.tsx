3c5ee89cfc70ea2f5d5c38862110af7535d276cb
Vector <davidoluremi0@gmail.com>
2025-02-01 12:26:15 +0100

    feat: integrate TodoCard component and add dark mode background support

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 // import { useState } from 'react'
 // import './App.css'
 
+import TodoCard from "./components/TodoCard"
+
+
 function App() {
 
   return (
     <>
-      <div className="bg-[url('/bg-desktop-light.jpg')]bg-contain bg-no-bg-repeat bg-center min-h-screen text-center">
-        <h1 className="text-4xl font-bold text-red-600">Hello World</h1>
+      <div className="bg-[url('/bg-desktop-light.jpg')] dark:bg-[url('/bg-desktop-dark.jpg')] bg-contain bg-no-repeat h-screen">
+        <TodoCard />
       </div>
     </>
   )
