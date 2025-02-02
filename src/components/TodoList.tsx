c46450e16bbba382125991dd3d9023263ef72d66
Vector <davidoluremi0@gmail.com>
2025-02-02 11:27:17 +0100

    fix: enhance checkbox styling and improve layout in TodoList component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -15,17 +15,34 @@ const TodoList: React.FC<TodoListProps> = ({ todos, toggleTodo }) => {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        {/* TodoList */}
         {todos.map(todo => (
           <div key={todo.id} className="p-4 text-gray-600 dark:text-gray-300">
-            <label className="flex items-center space-x-2">
-              <input 
-                type="checkbox"
-                checked={todo.completed}
-                onChange={() => toggleTodo(todo.id)}
-                className="appearance-none rounded-full h-5 w-5 border border-gray-300 focus:outline-none checked:bg-gradient-to-r checked:from-purple-500 checked:to-blue-500 checked:border-transparent checked:relative checked:before:content-['/'] checked:before:absolute checked:before:inset-0 checked:before:flex checked:before:items-center checked:before:justify-center checked:before:text-white" 
-              />
-              <span className={todo.completed ? "line-through text-gray-400" : ""}>{todo.text}</span>
+            <label className="flex items-center space-x-3 relative">
+              <div className="relative flex items-center justify-center w-6 h-6">
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => toggleTodo(todo.id)}
+                  className="appearance-none rounded-full h-5 w-5 border border-gray-300 focus:outline-none 
+                             checked:bg-gradient-to-br checked:from-CheckBlue checked:to-CheckPurple
+                             checked:border-transparent"
+                />
+                {todo.completed && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="absolute left-1.5 top-1.5 w-3 h-3 text-white"
+                    viewBox="0 0 11 9"
+                    fill="none"
+                    stroke="white"
+                    strokeWidth="2"
+                  >
+                    <path d="M1 4.304L3.696 7l6-6" />
+                  </svg>
+                )}
+              </div>
+              <span className={todo.completed ? "line-through text-gray-400" : ""}>
+                {todo.text}
+              </span>
             </label>
           </div>
         ))}
@@ -34,4 +51,4 @@ const TodoList: React.FC<TodoListProps> = ({ todos, toggleTodo }) => {
   );
 };
 
-export default TodoList
+export default TodoList;
